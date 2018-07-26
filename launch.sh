563d75bf43c3aaa10f5e3c980839444ee1c82e7e
jarvis <leguroky@gmail.com>
2018-07-26 22:57:08 +0800

    refine; reset data

--- launch.sh
+++ launch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-set -ex
+set -x
+
+git log -1 HEAD --pretty=format:%s | grep reset
+
+if [ $? -eq 0 ]; then
+	echo 'reset'
+	#sh resetData4Dev.sh
+fi
 
 ./manage.py migrate && \
 	./manage.py runserver 0.0.0.0:8000
