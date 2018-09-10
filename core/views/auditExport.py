f9776173345d370e78c9a3a7e7656db6de3827e8
jarvis <leguroky@gmail.com>
2018-09-10 16:03:07 +0800

    fix bug

--- core/views/auditExport.py
+++ core/views/auditExport.py
@@ -357,6 +357,35 @@ def exportLoanAuditDoc(activity):
     return path
 
 
+def set_border(ws, cell_range, border_style):
+    rows = ws[cell_range]
+    side = Side(border_style=border_style, color="00000000")
+
+    rows = list(rows)  # we convert iterator to list for simplicity, but it's not memory efficient solution
+    max_y = len(rows) - 1  # index of the last row
+    for pos_y, cells in enumerate(rows):
+        max_x = len(cells) - 1  # index of the last cell
+        for pos_x, cell in enumerate(cells):
+            border = Border(
+                left=cell.border.left,
+                right=cell.border.right,
+                top=cell.border.top,
+                bottom=cell.border.bottom
+            )
+            if pos_x == 0:
+                border.left = side
+            if pos_x == max_x:
+                border.right = side
+            if pos_y == 0:
+                border.top = side
+            if pos_y == max_y:
+                border.bottom = side
+
+            # set new border only if it's one of the edge cells
+            if pos_x == 0 or pos_x == max_x or pos_y == 0 or pos_y == max_y:
+                cell.border = border
+
+
 def exportMoneyAuditDoc(activity):
     wb = load_workbook(os.getcwd() + '/xlsx-templates/money.xlsx')
     ws = wb.active
@@ -417,6 +446,7 @@ def exportMoneyAuditDoc(activity):
 
         logger.info('fix border style {}'.format(cell.coord))
         style_range(ws, cell.coord, Border(top=thin, left=thin, right=thin, bottom=thin))
+    set_border(ws, 'A1:M13', 'medium')
 
     ws.protection.sheet = True
     ws.protection.set_password('zyhr2018')
