5d13d3773fd58217d8fc6d8e1adebaa7884e8a59
jarvis <leguroky@gmail.com>
2018-09-20 16:35:22 +0800

    merge migrations

--- core/migrations/0009_auto_20180920_0756.py
+++ core/migrations/0009_auto_20180920_0756.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0005_auto_20180821_0252'),
+        ('core', '0008_fincustomer_fincustomerops'),
     ]
 
     operations = [
--- core/migrations/0010_memo.py
+++ core/migrations/0010_memo.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0006_auto_20180920_0756'),
+        ('core', '0009_auto_20180920_0756'),
     ]
 
     operations = [
