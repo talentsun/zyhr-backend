15cd937a234ccd86d283f850e1737496722eae2b
jarvis <leguroky@gmail.com>
2018-11-06 11:48:38 +0800

    fix bug

--- core/management/commands/stats.py
+++ core/management/commands/stats.py
@@ -264,12 +264,22 @@ class Command(BaseCommand):
         stopMonth = self.calStopMonth()
         while month < stopMonth:
             monthText = month.strftime('%Y-%m')
+
             for customer in customers:
-                data = self.calCustomerStatsByCustomer(c, month=monthText)
-                CustomerStat.objects.create(category='month',
-                                            customer=customer,
-                                            month=monthText,
-                                            **data)
+                try:
+                    logger.info(
+                        "calculate customer stats by month: {} for customer {}".format(monthText, customer.name))
+                    data = self.calCustomerStatsByCustomer(c, month=monthText)
+                    CustomerStat.objects.create(category='month',
+                                                customer=customer,
+                                                month=monthText,
+                                                **data)
+                    logger.info("calculate customer stats by month: {} for customer {}, data: {}".format(monthText,
+                                                                                                         customer.name,
+                                                                                                         data))
+                except:
+                    logger.exception("fail to calculate customer stats")
+
             month = nextMonth
             nextMonth = self.calNextMonth(month)
 
