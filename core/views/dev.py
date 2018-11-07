3310dbe2fabb95d584bd414bf6a50786b08edda7
jarvis <leguroky@gmail.com>
2018-11-07 11:09:16 +0800

    fix bug

--- core/views/dev.py
+++ core/views/dev.py
@@ -10,7 +10,9 @@ logger = logging.getLogger('app.core.views.dev')
 
 def trigger_stats(request):
     task = AsyncTask.objects.create(category='stats', exec_at=timezone.now(), data={})
+
     while not task.finished:
+        task = AsyncTask.objects.get(pk=task.pk)
         time.sleep(1)
 
     return JsonResponse({'ok': True})
