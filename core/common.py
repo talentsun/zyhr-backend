7c4306766e71be3648088045c291936b5606ec03
jarvis <leguroky@gmail.com>
2019-03-12 09:48:44 +0800

    save

--- core/common.py
+++ core/common.py
@@ -63,6 +63,7 @@ def resolve_profile(profile,
         messages = Message.objects \
             .filter(profile=profile, read=False) \
             .order_by('-updated_at')
+        messages = messages[0:50]
         result['messages'] = [{
             'id': str(m.pk),
             'read': m.read,
