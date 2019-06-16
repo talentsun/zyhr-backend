d63464ebecc11295f3741a49f09a8186678cd585
whatever <whatever@gmail.com>
2019-06-16 23:40:44 +0800

    save

--- core/views/auditExportPDF.py
+++ core/views/auditExportPDF.py
@@ -198,6 +198,112 @@ def exportBizContractAuditPDF(activity):
     return ret
 
 
+def exportZizhishiyongAuditPDF(activity):
+    auditData = activity.extra
+
+    ret = resolve_activity(activity)
+    ret['createdAt'] = activity.created_at.strftime('%Y-%m-%d')
+
+    usage = activity.extra['usage']
+    auditData['shifoudaichu'] = '是否带出：{}'.format('是' if usage['out'] == '1' else '否')
+
+    date = usage['date'] if 'date' in usage else '无'
+    member = usage['member'] if 'member' in usage else '无'
+    desc = usage['desc'] if 'desc' in usage else '无'
+    auditData['yujiguihuan'] = '1.预计归还时间：{}             2. 陪同人员：{}\n3.其他说明事项：{}'.format(date, member, desc)
+
+    return ret
+
+
+def exportZichancaigouAuditPDF(activity):
+    auditData = activity.extra
+
+    ret = resolve_activity(activity)
+    ret['createdAt'] = activity.created_at.strftime('%Y-%m-%d')
+
+    items = auditData['items']
+    for index, item in enumerate(items):
+        item['jine'] = amountFixed(try_convert_float(item['price']) * try_convert_float(item['num']))
+
+    return ret
+
+
+def exportZichanbaofeiAuditPDF(activity):
+    auditData = activity.extra
+
+    ret = resolve_activity(activity)
+    ret['createdAt'] = activity.created_at.strftime('%Y-%m-%d')
+
+    return ret
+
+
+def exportDanganjiechuAuditPDF(activity):
+    ret = resolve_activity(activity)
+    ret['createdAt'] = activity.created_at.strftime('%Y-%m-%d')
+
+    return ret
+
+
+def exportYinjiankezhiAuditPDF(activity):
+    ret = resolve_activity(activity)
+    ret['createdAt'] = activity.created_at.strftime('%Y-%m-%d')
+
+    return ret
+
+
+def exportKaoqinyichangAuditPDF(activity):
+    ret = resolve_activity(activity)
+    ret['createdAt'] = activity.created_at.strftime('%Y-%m-%d')
+
+    info = activity.extra
+    startDate = datetime.datetime.strptime(info['date'][0], "%Y-%m-%dT%H:%M:%S.%fZ") + datetime.timedelta(hours=8)
+    endDate = datetime.datetime.strptime(info['date'][1], "%Y-%m-%dT%H:%M:%S.%fZ") + datetime.timedelta(hours=8)
+    dayStr = '自' + startDate.strftime('%Y-%m-%d %H:%M') + '到' + endDate.strftime('%Y-%m-%d %H:%M')
+    info['daterange'] = dayStr
+
+    return ret
+
+
+def exportChuchaiAuditPDF(activity):
+    ret = resolve_activity(activity)
+    ret['createdAt'] = activity.created_at.strftime('%Y-%m-%d')
+
+    return ret
+
+
+def exportQingjiaAuditPDF(activity):
+    info = activity.extra
+    ret = resolve_activity(activity)
+    ret['createdAt'] = activity.created_at.strftime('%Y-%m-%d')
+
+    typeOptions = [
+        {'id': 1, 'name': "事假"},
+        {'id': 2, 'name': "病假"},
+        {'id': 3, 'name': "工伤假"},
+        {'id': 4, 'name': "年休假"},
+        {'id': 5, 'name': "婚假"},
+        {'id': 6, 'name': "产假"},
+        {'id': 7, 'name': "产检假"},
+        {'id': 8, 'name': "看护假"},
+        {'id': 9, 'name': "哺乳假"},
+        {'id': 10, 'name': "丧假"},
+        {'id': 11, 'name': "考试假"},
+        {'id': 100, 'name': "其他"}
+    ]
+    for o in typeOptions:
+        if info['type'] == o['id']:
+            info['typeName'] = o['name']
+
+    startDate = datetime.datetime.strptime(info['date'][0], "%Y-%m-%dT%H:%M:%S.%fZ") + datetime.timedelta(hours=8)
+    endDate = datetime.datetime.strptime(info['date'][1], "%Y-%m-%dT%H:%M:%S.%fZ") + datetime.timedelta(hours=8)
+    dateStr = startDate.strftime('%Y-%m-%d %H:%M') + ' - ' + endDate.strftime('%Y-%m-%d %H:%M')
+    if 'days' in info:
+        dateStr = dateStr + '   ' + '共 {} 天'.format(info['days'])
+    info['datestr'] = dateStr
+
+    return ret
+
+
 def exportPDFData(activity):
     payload = None
     if activity.config.subtype == 'open_account':
@@ -220,30 +326,22 @@ def exportPDFData(activity):
     #    # yongren
     #    path = exportYongrenAuditDoc(activity)
     #    filename = '用人需求审批单.xlsx'
-    # elif activity.config.subtype == 'qingjia':
-    #    path = exportQingjiaAuditDoc(activity)
-    #    filename = '请假申请审批单.xlsx'
-    # elif activity.config.subtype == 'chuchai':
-    #    path = exportChuchaiAuditDoc(activity)
-    #    filename = '出差申请审批单.xlsx'
-    # elif activity.config.subtype == 'kaoqin_yichang':
-    #    path = exportKaoqinyichangAuditDoc(activity)
-    #    filename = '考勤异常申请审批单.xlsx'
-    # elif activity.config.subtype == 'zizhi_shiyong':
-    #    path = exportZizhishiyongAuditDoc(activity)
-    #    filename = '资质使用申请审批单.xlsx'
-    # elif activity.config.subtype == 'yinjian_kezhi':
-    #    path = exportYinjiankezhiAuditDoc(activity)
-    #    filename = '印鉴刻制申请审批单.xlsx'
-    # elif activity.config.subtype == 'dangan_jiechu':
-    #    path = exportDanganjiechuAuditDoc(activity)
-    #    filename = '业务档案原件借出申请审批单.xlsx'
-    # elif activity.config.subtype == 'zichan_baofei':
-    #    path = exportZichanbaofeiAuditDoc(activity)
-    #    filename = '资产报废申请审批单.xlsx'
-    # elif activity.config.subtype == 'zichan_caigou':
-    #    path = exportZichancaigouAuditDoc(activity)
-    #    filename = '资产购置申请审批单.xlsx'
+    elif activity.config.subtype == 'qingjia':
+        payload = exportQingjiaAuditPDF(activity)
+    elif activity.config.subtype == 'chuchai':
+        payload = exportChuchaiAuditPDF(activity)
+    elif activity.config.subtype == 'kaoqin_yichang':
+        payload = exportKaoqinyichangAuditPDF(activity)
+    elif activity.config.subtype == 'zizhi_shiyong':
+        payload = exportZizhishiyongAuditPDF(activity)
+    elif activity.config.subtype == 'yinjian_kezhi':
+        payload = exportYinjiankezhiAuditPDF(activity)
+    elif activity.config.subtype == 'dangan_jiechu':
+        payload = exportDanganjiechuAuditPDF(activity)
+    elif activity.config.subtype == 'zichan_baofei':
+        payload = exportZichanbaofeiAuditPDF(activity)
+    elif activity.config.subtype == 'zichan_caigou':
+        payload = exportZichancaigouAuditPDF(activity)
     # elif activity.config.subtype == 'zhuanzheng':
     #    path = exportZhuanzhengAuditDoc(activity)
     #    filename = '员工转正评定审批单.xlsx'
